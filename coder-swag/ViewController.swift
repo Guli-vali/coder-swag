174082a6e30ebb19c56430913cb13de5d13cf884
John Nelsen <ilamasliev@ilas-macbook-air.local>
2018-02-07 20:26:12 +0300

    Initial Commit

---
+++ coder-swag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Илья Маслиев on 07/02/2018.
+//  Copyright © 2018 Masliev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
